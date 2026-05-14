e5bb3bacd73ec7c2e90b68ba515a7dc37617a265
Emanuelle <emanuellenmoura@gmail.com>
2026-05-14 16:32:00 -0300

    estruturas de dado de auxilio

---
+++ utils/structs.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, state, parent, action, path_cost):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+
+class QueueFrontier:
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty(): raise Exception("empty frontier")
+        node = self.frontier[0]
+        self.frontier = self.frontier[1:]
+        return node
